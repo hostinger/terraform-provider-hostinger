4315562576e51f40311eb58924480824bc48f2ec
Merkys <merkys.maliukevicius@hostinger.com>
2025-07-07 15:48:29 +0300

    fix(provider): update version number

--- hostinger/provider.go
+++ hostinger/provider.go
@@ -49,6 +49,6 @@ func providerConfigure(_ context.Context, d *schema.ResourceData) (interface{}, 
 	}
 
 	// Initialize the Hostinger API client
-	client := NewHostingerClient(token, "0.1.17")
+	client := NewHostingerClient(token, "0.1.18")
 	return client, diags
 }
